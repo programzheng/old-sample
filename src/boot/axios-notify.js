2d2f69a6be2c8422ab26f8abbe58cd5d3f622e5b
neil <esz759486@gmail.com>
2019-08-24 23:30:40 +0800

    axios catch response error pop notify

---
+++ src/boot/axios-notify.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+// quasar toast
+import { Notify } from 'quasar'
+
+export default async ({ Vue }) => {
+  Notify.setDefaults({
+    position: 'top',
+    timeout: 2500,
+    color: 'red-10',
+    textColor: 'white',
+    actions: [{ icon: 'close', color: 'white' }]
+  })
+
+  const instance = axios.create({
+    baseURL: process.env.API,
+    timeout: 1000,
+    headers: {
+      'accept': 'application/json',
+      'content-type': 'application/json'
+    }
+  })
+  // response handler
+  instance.interceptors.response.use(function (response) {
+    if (response.data.Result.Error !== null) {
+      Notify.create(response.data.Result.Error)
+      return new Promise(() => {})
+    }
+  })
+  Vue.prototype.$axios = instance
+}
