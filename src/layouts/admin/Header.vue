dc19f7ad85691b723fde50409f5c2fcc7e6d7715
neil <esz759486@gmail.com>
2019-10-06 16:19:52 +0800

    admin 切分header跟menu

---
+++ src/layouts/admin/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-header elevated>
+    <q-toolbar>
+      <q-btn v-if="toolbarButton"
+        flat
+        dense
+        round
+        @click="leftDrawerOpen = !leftDrawerOpen"
+        aria-label="Menu"
+      >
+        <q-icon name="menu" />
+      </q-btn>
+
+      <q-toolbar-title>
+        Quasar App
+      </q-toolbar-title>
+
+      <div><q-btn @click="logout">登出</q-btn></div>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+export default {
+  name: 'AdminHeader',
+  data () {
+    return {
+      toolbarButton: false,
+      leftDrawerOpen: false
+    }
+  },
+  methods: {
+    logout () {
+      this.$q.cookies.remove('token')
+      this.$router.push({ path: 'admin/login' })
+    }
+  }
+}
+</script>
